49105eb6df9e53b726110ca0909cafab6e3d58b8
CayShea <cayleesheaharrington@gmail.com>
2020-02-24 18:11:46 -0800

    initial commit

---
+++ src/components/ToDo.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <header>
+            <h1>Todo</h1>
+            <input placeholder="what needs to be done?" v-model="newTask" @keyup.enter="addTask($event)"/>
+        </header>
+
+        <div>
+            <ul>
+                <li 
+                    v-for="task in tasks"
+                    v-bind:key="task.id"
+                    :class="{ 'is-done': task.done}"
+                >
+                    <input type="checkbox" v-model="task.done" @click="check"/>
+                    {{ task.text }}
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+
+
+<script>
+export default {
+    data() {
+        return {
+            newTask: '',
+            tasks: [
+                {
+                    text: "Learn Vue",
+                    done: false
+                },
+                {
+                    text: "Become a Software Engineer",
+                    done: false
+                }
+            ]
+        }  
+    },
+    
+    methods: {
+        addTask(event) {
+            event.preventDefault();
+            this.tasks.push({text: this.newTask, done: false})
+            this.newTask = ''
+        },
+        check(task) {
+            task.done = true
+        } 
+    }
+    
+}
+</script>
+
+
+
+<style scoped>
+.is-done {
+    text-decoration: line-through
+}
+</style>
